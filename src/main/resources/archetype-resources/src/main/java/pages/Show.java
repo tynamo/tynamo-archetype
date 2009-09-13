dc7bcda3b15e53cfeff5826bb436d6f2c73a3cea
Alejandro Scandroli <ascandroli@codehaus.org>
2009-09-13 07:02:41 +0000

    finishing up the Show page

---
+++ src/main/resources/archetype-resources/src/main/java/pages/Show.java
@@ -0,0 +1,36 @@
+package ${package}.pages;
+
+
+import org.apache.tapestry5.Link;
+import org.trailsframework.hibernate.pages.HibernateModelPage;
+import org.trailsframework.util.DisplayNameUtils;
+
+public class Show extends HibernateModelPage
+{
+
+	public Object[] getEditPageContext()
+	{
+		return new Object[]{getClassDescriptor().getType(), getBean()};
+	}
+
+	public Link back()
+	{
+		return getResources().createPageLink(List.class, false, getClassDescriptor().getType());
+	}
+
+	public String getEditLinkMessage()
+	{
+		return getMessages().format("org.trails.i18n.edit", DisplayNameUtils.getDisplayName(getClassDescriptor(), getMessages()));
+	}
+
+	public String getTitle()
+	{
+		return getMessages().format("org.trails.i18n.show", getBean().toString(), getMessages());
+	}
+
+	public Link onActionFromDelete()
+	{
+		getPersitenceService().remove(getBean());
+		return back();
+	}
+}
